b079c0549ff37d5ed24dbdf9772eddd88bdccf1a
sbwml <admin@cooluc.com>
2023-07-24 12:36:49 +0800

    luci-app-daed: add logging support

--- luasrc/model/cbi/daed/basic.lua
+++ luasrc/model/cbi/daed/basic.lua
@@ -4,7 +4,7 @@ m = Map("daed")
 m.title = translate("DAED")
 m.description = translate("DAE is a Linux high-performance transparent proxy solution based on eBPF, And DAED is a modern dashboard for dae.")
 
-m:section(SimpleSection).template = "daed_status"
+m:section(SimpleSection).template = "daed/daed_status"
 
 s = m:section(TypedSection, "daed", translate("Global Settings"))
 s.addremove = false
@@ -12,6 +12,12 @@ s.anonymous = true
 
 o = s:option(Flag,"enabled",translate("Enable"))
 o.default = 0
+
+o = s:option(Value, "log_maxbackups", translate("Logfile retention count"))
+o.default = 1
+
+o = s:option(Value, "log_maxsize", translate("Logfile Max Size (MB)"))
+o.default = 5
 
 o = s:option(Value, "listen_addr",translate("Set the DAED listen address"))
 o.default = '0.0.0.0:2023'
